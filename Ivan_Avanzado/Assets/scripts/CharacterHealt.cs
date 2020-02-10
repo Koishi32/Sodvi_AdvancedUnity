4dde325f3f45eadc52c8b4e5ca049be49aa0de6c
Ivan_3 <alejandroivanleyvadiaz@gmail.com>
2020-02-09 22:40:59 -0600

    Addition1
    An improved map was added
    character returns to live

---
+++ Ivan_Avanzado/Assets/scripts/CharacterHealt.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class CharacterHealt : MonoBehaviour
+{
+    public Image lifeBar;
+    public float health=50f;
+    private float maxLHealth;
+    public Transform pos_ini;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        maxLHealth=health;
+        //pos_ini = this.transform.position;
+    }
+    private void Update()
+    {
+        if (this.transform.position.y < -50 || health <= 1f)
+        {
+            resert();
+        }
+    }
+    public void TakeDamage(float amount){
+        health-=amount;
+        lifeBar.fillAmount=health/maxLHealth;
+
+    }
+    public void resert()
+    {
+        health = maxLHealth;
+        lifeBar.fillAmount = 100;
+        this.transform.position = pos_ini.position;
+    }
+
+}
